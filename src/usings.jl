e54f735f072611937ae6ddce26310b3cf4428c55
Quentin Burthier <quentin.burthier@ensta-paris.fr>
2019-12-13 18:29:55 +0100

    last changes

--- src/usings.jl
+++ src/usings.jl
@@ -1,6 +1,8 @@
-# Ce fichier doit être chargé **après** l'appel du main().
-# On connait alors les options et on peut charger les solveurs
-# utiles en fonction de ces options.
+# Ce fichier n'est là que pour sélection et charer (par using) les packages
+# strictement indispensable en fonction des options passées au programme.
+# En effet les packages tels que JuMP, CPLE, Cbc, ... sont assez lents à charger
+# et ne sont pas toujours indispensable selon les option.
+# Par conséquent, ce fichier ne peut-être charger que après analyse des options.
 #
 # Les choix de solveur (PLNE ou PL) sont :
 # - avec les actions :mip ou :dmip : tout est fait en PLNE
@@ -8,13 +10,14 @@
 # - sinon la solution sous-traite le sous-problème de timing (positionner les
 #   avions avec ordre imposé) à un solveur qui peut être :
 #   - soit algorithmique pur (timing_algo_solver==:dynprog ou :earliest)
-#   - soit par solveur PL (et pas PLNE). Dans ce cas les solveurs sont
-#     cplex, glpk ou clp (clp à la place de cbc)
+#   - soit par solveur PL (et pas PLNE) (timing_algo_solver==:lp). 
+#     Dans ce cas les solveurs sont cplex, glpk ou clp (clp à la place de cbc)
 
 # On évite de sourcer ce même fichier plusieurs fois
 if @isdefined(USINGS_IS_LOADED)
     return
 end
+ln1("========================= USINGS BEGIN ($(ms()))")
 
 # Cette ligne ne fonctionne que si l'analyse des arguments est déjà faite.
 #
@@ -27,29 +30,19 @@ timing_algo_solver = Args.get("timing_algo_solver")
 # action demandée : elle nécessite ou non le pkg JuMP
 action = Args.get("action")
 
-# Inclusion de JuMP et des solvers externes seulement si nécessaire
+# Nécessité de JuMP ?
 need_jump = false
 # On peut avoir plusieurs classe LpTimingSolver (Lp2TimingSolver, ...)
 use_timing_algo_solver = occursin(r"^lp", String(timing_algo_solver))
+# Cas d'un résolution globales par approche PLNE frontale.
 if (action in [:mip, :dmip] || use_timing_algo_solver )
     need_jump = true
 end
 
-# ulg pour usings::log (pour éviter de surcharger un lg par ailleurs)
-# La première ligne évite les hurlements en cas de rechargement de ce fichier.
-!@isdefined(ulg) && # la suite n'est effectuée qui le bool précédent est true.
-function ulg(str)
-    # if Args.get("level") >= 2
-    if Args.get("level") >= 1
-        println("usings.jl: $str")
-    end
-end
-
-ulg("========================= USINGS BEGIN ($(ms()))")
-ulg("action=$action")
-ulg("need_jump=$need_jump")
-ulg("external_mip_solver=$external_mip_solver")
-ulg("timing_algo_solver=$timing_algo_solver")
+ln1("action=$action")
+ln1("need_jump=$need_jump")
+ln1("external_mip_solver=$external_mip_solver")
+ln1("timing_algo_solver=$timing_algo_solver")
 
 if need_jump
     @ms using JuMP
@@ -76,4 +69,4 @@ if timing_algo_solver == :earliest
 end
 
 USINGS_IS_LOADED = true
-ulg("========================= USINGS END ($(ms()))")
+ln1("========================= USINGS END ($(ms()))")
