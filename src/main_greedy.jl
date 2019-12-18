3350c784b4dfbf4828252b20e7a55f9736ffe6b3
AntoineTHO <58596727+AntoineTHO@users.noreply.github.com>
2019-12-18 17:47:34 +0100

    Update main_greedy.jl
    Ajout du coût de la solution
    Création du fichier solution
    Ajout des données dans ce fichier

--- src/main_greedy.jl
+++ src/main_greedy.jl
@@ -24,6 +24,7 @@ function main_greedy(args)
 
     perm = sortperm(arrivals)
     planes = planes[perm]
+
     arrivals = arrivals[perm]
     departs = departs[perm]
 
@@ -39,16 +40,50 @@ function main_greedy(args)
     x[1] = arrivals[1]
 
     for i=2:n
-        if arrivals[i] > arrivals[i - 1] + sep[i-1]
-            x[i] = arrivals[i]
-        else
-            x[i] = arrivals[i-1] + sep[i-1]
-        end
+        x[i] = max(arrivals[i], x[i - 1] + sep[i-1]) #dans l'idéal il faudrait boucler sur TOUS les avions précédant i, car on peut respecter la distance avec i-1 mais pas i-2 par exemple
     end
 
     println(x)
 
     println("Fin de l'action greedy")
+
+    # Calcul du cout
+
+    cost = 0
+
+    costvect = zeros(n)
+    for i=1:n
+        delta = x[i] - arrivals[i] # positive if lands later than expected
+        costvect[i] = 10 * min(delta,0) + 30 * max(delta,0) #pénalité de 10 par unité d'avance, 30 par unité de retard
+        cost = cost + costvect[i]
+    end
+
+    #Création du fichier solution 
+
+    solfilepath = string("$APPDIR/sols/",instance.name,"=",cost,".sol")
+    println(solfilepath)
+    io = open(solfilepath, "w")
+
+    # Remplissage du fichier solution 
+
+    println(io,string("name ",instance.name))#,"\n"))
+    #pas besoin de sauter de ligne, c'est automatique lorsque l'on fait un nouveau println
+    println(io,string("timestamp ",Dates.DateTime(now())))
+    println(io,string("cost ", cost))
+    println(io,string("order ",planes))
+    println(io,"")
+    println(io,"#       name     t   dt  cost ")#       # comments")
+
+    # boucle for sur les avions
+
+    for i = 1:n
+        println(io,string(
+        "landing     ",planes[i]," ",x[i]," ",x[i]-arrivals[i]," ",costvect[i]," "
+        ))
+    end
+
+    close(io)
+    
 end
 
 main_greedy(Args.args)
