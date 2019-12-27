469ed694795835d7dadc7bdf70dc8c1afb94f29d
Quentin Burthier <quentin.burthier@ensta-paris.fr>
2019-12-27 23:48:08 +0100

    fix(greedy): target instead of lb, correct type at assignement

--- src/main_greedy.jl
+++ src/main_greedy.jl
@@ -5,47 +5,31 @@ function main_greedy(args)
     println("Début de l'action greedy")
 
     filename = args[:infile]
-
-    # println("Action greedy non implanté : AU BOULOT :-)")
-    # exit(1)
-    # error("\n\nAction greedy non implanté : AU BOULOT :-)\n\n")
     instance = Instance(filename)
+
     n = instance.nb_planes
-    planes = instance.planes
 
-    arrivals = zeros(n)
-    departs = zeros(n)
+    targets = [plane.target for plane in instance.planes]
 
-    for i = 1:n
-        plane = planes[i]
-        arrivals[i] = plane.lb
-        departs[i] = plane.ub
-    end
+    σ = sortperm(targets)
+    sorted_planes = instance.planes[σ]
+    targets = targets[σ]
 
-    perm = sortperm(arrivals)
-    planes = planes[perm]
+    sep = [get_sep(instance, sorted_planes[i], sorted_planes[i+1])
+           for i in 1:n-1]
 
-    arrivals = arrivals[perm]
-    departs = departs[perm]
-
-    sep = zeros(n-1)
-    for i = 1:n-1
-        sep[i] = get_sep(instance, planes[i], planes[i+1])
-    end
-
-    println(arrivals)
+    println(targets)
     println(sep)
 
     x = zeros(Int, n)
-    x[1] = arrivals[1]
-
+    x[1] = targets[1]
     for i=2:n
         # dans l'idéal il faudrait boucler sur TOUS les avions précédant i,
         # car on peut respecter la distance avec i-1 mais pas i-2 par exemple
-        x[i] = max(arrivals[i], x[i - 1] + sep[i-1])
+        x[i] = max(targets[i], x[i-1] + sep[i-1])
     end
 
-    solution = Solution(instance, planes, x)
+    solution = Solution(instance, sorted_planes, x)
     write(solution)
     print_sol(solution)
     println("Fin de l'algorithme glouton")
