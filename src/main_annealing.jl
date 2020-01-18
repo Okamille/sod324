08c40295017ef084377e79db416c590b3f4e68fa
Quentin Burthier <quentin.burthier@ensta-paris.fr>
2020-01-18 10:42:31 +0100

    feat(annealing): solver constructor

--- src/main_annealing.jl
+++ src/main_annealing.jl
@@ -15,23 +15,12 @@ function main_annealing(args)
     ln1("Solution initiale envoyée au solver")
     ln1(to_s(sol))
 
-
-    # Choix des options pour le solver
-    user_opts = Dict(
-        # :startsol           => nothing,  # nothing pour auto à partir de l'instance
-        :startsol           => sol,  # nothing pour auto à partir de l'instance
-        :step_size          => inst.nb_planes,   # à renommer en step_size
-        # :temp_init          => -1.0, # -1.0 pour automatique
-        :temp_init          => nothing, # nothing pour automatique
-        :temp_init_rate     => 0.30,  # valeur standard : 0.8
-        :temp_mini          => 0.000_001,
-        # :temp_coef          => 0.999_95,
-        :temp_coef          => 0.95,
-        :nb_cons_reject_max => 1_000_000_000, # infini
-        # :nb_cons_no_improv_max => 500*inst.size*inst.size,
-        :nb_cons_no_improv_max => 5000*inst.nb_planes,
+    sv = AnnealingSolver(
+        inst; 
+        temp_init_rate=0.3,
+        step_size=inst.nb_planes,
+        startsol=sol,
     )
-    sv = AnnealingSolver(inst, user_opts)
     ln1(get_stats(sv))
 
     ms_start = ms() # nb secondes depuis démarrage avec précision à la ms
