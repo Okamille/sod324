55e23446906cec2c52f15a1f9c322c8777d618f1
Quentin Burthier <quentin.burthier@ensta-paris.fr>
2020-01-24 18:48:10 +0100

    feat(greedy solver): overloaded to take a solution as input

--- src/solvers/greedy.jl
+++ src/solvers/greedy.jl
@@ -16,11 +16,7 @@ function greedy(instance::Instance)
     targets = targets[σ]
 
     sep = [get_sep(instance, sorted_planes[i], sorted_planes[i+1])
-        for i in 1:n-1]
-
-    println(targets)
-    println(sep)
-
+           for i in 1:n-1]
     x = zeros(Int, n)
     x[1] = targets[1]
     for i=2:n
@@ -29,4 +25,18 @@ function greedy(instance::Instance)
         x[i] = max(targets[i], x[i-1] + sep[i-1])
     end
     return Solution(instance, sorted_planes, x)
+end
+
+function greedy!(sol::Solution; presort=:ARGS)
+    n = sol.inst.nb_planes
+    initial_sort!(sol, presort=presort, solve=false)
+
+    sep = [get_sep(sol.inst, first_plane, second_plane)
+           for (first_plane, second_plane) in zip(sol.planes[1:end-1], sol.planes[2:end])]
+
+    sol.x[1] = sol.planes[1].target
+    for i=2:n
+        sol.x[i] = max(sol.planes[i].target, sol.x[i-1] + sep[i-1])
+    end
+    update_costs!(sol)
 end
